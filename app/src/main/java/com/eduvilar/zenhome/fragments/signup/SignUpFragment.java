99fbf471bff2812eb9b89824f82dc2670a48a3e4
evilar19 <eduardovilar10@gmail.com>
2017-11-27 23:34:24 +0100

    login sign up

---
+++ app/src/main/java/com/eduvilar/zenhome/fragments/signup/SignUpFragment.java
@@ -0,0 +1,67 @@
+package com.eduvilar.zenhome.fragments.login;
+
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.eduvilar.zenhome.R;
+import com.eduvilar.zenhome.base.BaseFragment;
+import com.eduvilar.zenhome.fragments.flat.FlatContract;
+import com.eduvilar.zenhome.fragments.flat.FlatPresenter;
+
+/**
+ * Created by eduardovilar10 on 25/11/2017.
+ */
+
+public class LoginFragment extends BaseFragment implements LoginContract.View {
+
+    private LoginContract.Presenter presenter;
+
+    private LinearLayout loadingView;
+    private LinearLayout contentView;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public int layout() {
+        return R.layout.fragment_flat;
+    }
+
+    @Override
+    public void view(View view) {
+        loadingView = (LinearLayout) view.findViewById(R.id.loading_view);
+        contentView = (LinearLayout) view.findViewById(R.id.content_view);
+    }
+
+    @Override
+    public void init() {
+        if (presenter == null) {
+            presenter = new LoginPresenter(this);
+        }
+        presenter.init();
+    }
+
+    @Override
+    public void success(String name) {
+
+    }
+
+    @Override
+    public void error(String error) {
+
+    }
+
+    @Override
+    public void onViewLoading() {
+        loadingView.setVisibility(View.VISIBLE);
+        contentView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onViewReady() {
+        loadingView.setVisibility(View.GONE);
+        contentView.setVisibility(View.VISIBLE);
+    }
+
+}
---
+++ app/src/main/java/com/eduvilar/zenhome/fragments/signup/SignUpPresenter.java
@@ -0,0 +1,45 @@
+package com.eduvilar.zenhome.fragments.login;
+
+import android.os.Handler;
+
+import com.eduvilar.zenhome.callback.LoginCallback;
+import com.eduvilar.zenhome.data.GetData;
+import com.eduvilar.zenhome.fragments.flat.FlatContract;
+import com.eduvilar.zenhome.model.User;
+
+/**
+ * Created by eduardovilar10 on 25/11/2017.
+ */
+
+public class LoginPresenter implements LoginContract.Presenter {
+
+    private LoginContract.View view;
+    private GetData getData;
+
+    public LoginPresenter(LoginContract.View view) {
+        this.view = view;
+        getData = new GetData();
+    }
+
+    @Override
+    public void init() {
+        view.onViewReady();
+    }
+
+    @Override
+    public void login(String email, String password) {
+        getData.logInUser(email, password, new LoginCallback() {
+            @Override
+            public void success(User user) {
+
+            }
+
+            @Override
+            public void fail(String error) {
+
+            }
+        });
+    }
+
+
+}
